35b4f87b82da1bbfa40f856fe03ef5e3e82ff3ec
Vladislav <vladvoronaimp@gmail.com>
2023-11-22 15:48:22 +0200

    Add lab 3

---
+++ lab_03/StudentList.py
@@ -0,0 +1,84 @@
+from Student import Student
+
+class StudentList:
+	def __init__(self):
+		self.students = []
+
+	def addNewElement(self):
+		name = input("Please enter student name: ")
+		phone = input("Please enter student phone: ")
+
+		while True:
+			try:
+				age = int(input("Please enter student age: "))
+				break
+			except ValueError:
+				print("Wrong student age")
+
+		email = input("Please enter student email: ")
+		student = Student(name, phone, age, email)
+		self.students.append(student)
+		self.students.sort(key=lambda x: (x.name, x.age))
+		print("New element has been added")
+
+	def deleteElement(self, name):
+		for student in self.students:
+			if name == student.name:
+				self.students.remove(student)
+				break
+		print("Element was not found")
+
+	def updateElement(self, name):
+		found = False
+		for student in self.students:
+			if name == student.name:
+				found = True
+				print(f"Student {name} found. Please update the information:")
+				deletePosition = self.students.index(student)
+				break
+
+		if not found:
+			print("Student not found")
+			return
+
+		del self.students[deletePosition]
+
+		nname = input("Please enter new name: ")
+		if nname == "":
+			nname = student.name
+
+		insertPosition = 0
+		for item in self.students:
+			if nname > item.name:
+				insertPosition += 1
+			else:
+				break
+
+		nstudent = Student(nname, student.phone, student.age, student.email)
+		self.students.insert(insertPosition, nstudent)
+
+		nphone = input("Please enter updated phone: ")
+		if nphone != "":
+			nstudent.phone = nphone
+			
+		while True:
+			try:
+				nage = input("Please enter updated age: ")
+				if nage != "":
+					nstudent.age = int(nage)
+				break
+			except ValueError:
+				print("Wrong student age")
+
+		nemail = input("Please enter updated email: ")
+		if nemail != "":
+			nstudent.email = nemail 
+
+		print("Element has been updated")
+
+
+	def printAllList(self):
+		for student in self.students:
+			str_for_print = f"Student name is {student.name}, Phone is {student.phone}, Age is {student.age}, Email is {student.email}"
+			print(str_for_print)
+		
---
+++ lab_03/test_lab_03.py
@@ -0,0 +1,82 @@
+import pytest
+from pathlib import Path
+from io import StringIO
+from unittest.mock import patch
+from lab_03 import main
+from FileHandler import FileHandler
+from StudentList import StudentList
+from Student import Student
+import csv
+
+@pytest.fixture
+def TestingData():
+	return [
+		{"name": "Bob", "phone": "1112233", "age": "20", "email": "bob@example.com"},
+		{"name": "Dilan", "phone": "2223344", "age": "21", "email": "Dilan@example.com"},
+		{"name": "Zak", "phone": "3334455", "age": "23", "email": "zak@example.com"}
+	]
+
+def test_FileLoad(TestingData, tmp_path):
+	PathFile = tmp_path / "lab3.csv"
+	with open(PathFile, "w", newline="", encoding="utf-8") as file:
+		ResFile = csv.DictWriter(file, fieldnames=["name", "phone", "age", "email"])
+		ResFile.writeheader()
+		ResFile.writerows(TestingData)
+
+	LData = FileHandler.LoadFile(str(PathFile), StudentList())
+	assert [vars(student) for student in LData.students] == TestingData
+
+def test_SaveFile(TestingData, tmp_path, capsys):
+	PathFile = tmp_path / "test_save_file.csv"
+	StudentL = StudentList()
+	StudentL.students = [Student(**data) for data in TestingData]
+
+	FileHandler.SaveFile(str(PathFile), StudentL)
+
+	captured = capsys.readouterr()
+	assert "The data is saved in" in captured.out
+
+	LData = FileHandler.LoadFile(str(PathFile), StudentList())
+	assert [vars(student) for student in LData.students] == [vars(student) for student in StudentL.students]
+
+def test_updateElement(tmp_path, capsys):
+	with patch('builtins.input', side_effect=["John2", "123", "30", "john@example.com"]):
+		StudentL = StudentList()
+		StudentL.students = [Student("John", "123", 30, "john@example.com")]
+	
+		with patch('sys.stdout', new_callable=StringIO) as TestInputAns:
+			StudentL.updateElement("John")
+	
+		captured = TestInputAns.getvalue()
+		assert "Element has been updated" in captured
+		assert vars(StudentL.students[0]) == {"name": "John2", "phone": "123", "age": 30, "email": "john@example.com"}
+
+def test_addNewElement(tmp_path):
+	with patch('builtins.input', side_effect=["Test", "123", "25", "test@example.com"]):
+		StudentL = StudentList()
+		StudentL.addNewElement()
+
+		assert len(StudentL.students) == 1
+		assert vars(StudentL.students[0]) == {"name": "Test", "phone": "123", "age": 25, "email": "test@example.com"}
+
+def test_deleteElement(capsys):
+	with patch('builtins.input', return_value="John"):
+		StudentL = StudentList()
+		StudentL.students = [Student("John", "123", 25, "john@example.com")]
+		
+		with patch('sys.stdout', new_callable=StringIO) as TestInputAns:
+			StudentL.deleteElement("John")
+
+		captured = TestInputAns.getvalue()
+		assert "Element was not found" in captured
+
+def test_printAllList(capsys):
+	StudentL = StudentList()
+	StudentL.students = [Student("John", "123", 25, "john@example.com")]
+
+	with patch('sys.stdout', new_callable=StringIO) as TestInputAns:
+		StudentL.printAllList()
+
+	captured = TestInputAns.getvalue()
+	assert "Student name is John, Phone is 123, Age is 25, Email is john@example.com" in captured
+
